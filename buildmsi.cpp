e3d6664d27d2b30af28ccd5eacd7ea417206044d
Simon Richter <Simon.Richter@hogyros.de>
2018-10-06 17:40:45 +0100

    COM Framework

--- buildmsi.cpp
+++ buildmsi.cpp
@@ -1,6 +1,24 @@
 #include <tchar.h>
 
+#include <objbase.h>
+
+#include <iostream>
+#include <iomanip>
+
 int _tmain(int, TCHAR **)
+try
 {
+	HRESULT rc = ::CoInitializeEx(0, COINIT_APARTMENTTHREADED);
+
+	if(FAILED(rc))
+		throw rc;
+
+	::CoUninitialize();
+
 	return 0;
+}
+catch (HRESULT rc)
+{
+	std::cerr << "Unhandled error " << std::hex << std::setw(8) << std::setfill('0') << rc << std::endl;
+	return 1;
 }
